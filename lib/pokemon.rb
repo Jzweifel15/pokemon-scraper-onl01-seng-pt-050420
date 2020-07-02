0af55a3092df3ba7557bf580a1816d0df7ce7a2a
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-02 14:41:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
   
   def initialize(id:, name:, type: db:)
-    @id = id if != nil 
+    @id = id
     @name = name 
     @type = type
     @db = db
